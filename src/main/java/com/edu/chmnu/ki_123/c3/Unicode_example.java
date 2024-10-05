20c6a33e53bd7022d6fac3a36da6c8da25ec7d4a
Nikita <nikitagulmamedov@gmail.com>
2024-10-05 18:27:17 +0300

    Init

---
+++ src/main/java/com/edu/chmnu/ki_123/c3/Unicode_example.java
@@ -0,0 +1,12 @@
+package com.edu.chmnu.ki_123.c3;
+
+public class Unicode_example {
+    public static void main(String[] args) {
+        char unicodeB = '\u00DF';
+        char unicodePowTwo = '\u00B2';
+
+        System.out.println("The sharp latin small latter is " + unicodeB);
+
+        System.out.println("The second pow is " + unicodePowTwo);
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        char unicodeB = '\u00DF';
+        char unicodePowTwo = '\u00B2';
+
+        System.out.println("The sharp latin small latter is " + unicodeB);
+
+        System.out.println("The second pow is " + unicodePowTwo);
+    }
+}
